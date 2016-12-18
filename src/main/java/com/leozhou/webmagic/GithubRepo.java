e5db9e42746997f7cd811e155456f5ccf5349b64
leozhou <492753662@qq.com>
2016-12-18 17:49:22 +0800

    init object

---
+++ src/main/java/com/leozhou/webmagic/GithubRepo.java
@@ -0,0 +1,34 @@
+package com.leozhou.webmagic;
+
+/**
+ * Created by zhouchunjie on 2016/12/18.
+ */
+public class GithubRepo {
+    private String name;
+    private String author;
+    private String readme;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getReadme() {
+        return readme;
+    }
+
+    public void setReadme(String readme) {
+        this.readme = readme;
+    }
+}
