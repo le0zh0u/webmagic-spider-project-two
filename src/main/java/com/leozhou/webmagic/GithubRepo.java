9b2e7400421bf14e731309dcda9e2a59b20817be
leozhou <492753662@qq.com>
2016-12-18 19:37:14 +0800

    update github repo object , but some dependencies are not found, will update later

--- src/main/java/com/leozhou/webmagic/GithubRepo.java
+++ src/main/java/com/leozhou/webmagic/GithubRepo.java
@@ -1,11 +1,25 @@
 package com.leozhou.webmagic;
 
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.model.ConsolePageModelPipeline;
+import us.codecraft.webmagic.model.OOSpider;
+import us.codecraft.webmagic.model.annotation.ExtractBy;
+import us.codecraft.webmagic.model.annotation.ExtractByUrl;
+import us.codecraft.webmagic.model.annotation.HelpUrl;
+import us.codecraft.webmagic.model.annotation.TargetUrl;
+
 /**
  * Created by zhouchunjie on 2016/12/18.
  */
+@TargetUrl("https://github.com/\\w+/\\w+")
+@HelpUrl("https://github.com/\\w+")
 public class GithubRepo {
+
+    @ExtractBy(value = "//h1[@class='entry-title public']/strong/a/text()", notNull = true)
     private String name;
+    @ExtractByUrl("https://github.com/(\\w+)/.*")
     private String author;
+    @ExtractBy("//div[@id='readme']/tidyText()")
     private String readme;
 
     public String getName() {
@@ -30,5 +44,9 @@ public class GithubRepo {
 
     public void setReadme(String readme) {
         this.readme = readme;
+    }
+
+    public static void main(String[] args){
+        OOSpider.create(Site.me().setSleepTime(1000), new ConsolePageModelPipeline(), GithubRepo.class).addUrl("https://github.com/code4craft").thread(5).run();
     }
 }
